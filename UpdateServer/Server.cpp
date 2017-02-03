0df2f9f691d6205db5bdb9afea0d7515de0fac09
shawngkelly <shawngkelly@hotmail.com>
2017-02-02 19:07:35 -0500

    Update

--- UpdateServer/Server.cpp
+++ UpdateServer/Server.cpp
@@ -11,7 +11,7 @@ const char IPADDR[] = "127.0.0.1";
 const int  PORT = 50000;
 const int  QUERY = 1;
 const int  UPDATE = 2;
-const int STRLEN = 256;
+const int  STRLEN = 256;
 
 // Closes the socket and performs the WSACleanup
 void cleanup(SOCKET socket);
@@ -20,7 +20,11 @@ void cleanup(SOCKET socket);
 int getLocalVersion();
 
 int main()
-{
+{	
+	//Update vars
+	int			serverVersion;
+
+	//Connection vars
 	WSADATA		wsaData;
 	SOCKET		listenSocket;
 	SOCKET		acceptSocket;
@@ -29,6 +33,10 @@ int main()
 	char		sendMessage[STRLEN];
 	char		recvMessage[STRLEN];
 	bool		done = false;
+
+	//Server checks local version number
+	serverVersion = getLocalVersion();
+
 
 	// Loads Windows DLL (Winsock version 2.2) used in network programming
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR)
@@ -81,21 +89,66 @@ int main()
 
 	cout << "Connected...\n\n";
 
-	int serverVersion = getLocalVersion();
+	
 	while (!done)
 	{
 		// Wait to receive a message from the remote computer
+
 		cout << "\n\t--WAIT--\n\n";
 		int vRecv = recv(acceptSocket, recvMessage, STRLEN, 0);
 		if (vRecv > 0)
 		{
 			recvMessage[vRecv] = '\0';
-			cout << "Checking incoming update of verison " << (int)recvMessage[0] << "\n";
-			cout << "Latest Version is " << serverVersion << "\n\n";
-			if ((int)recvMessage[0] == serverVersion)
-			{   
-				char noUpdate[22] = "No updates available";
-				int iSend = send(acceptSocket, noUpdate, strlen(noUpdate), 0);
+			if ((int)recvMessage[0] == 1)
+			{
+				cout << "Client is requesting server version.\n\n";
+				cout << "Latest Version is " << serverVersion << "\n\n";
+				cout << "Sending version number now\n";
+				int iSend = send(acceptSocket, (char*)&serverVersion, strlen((char*)&serverVersion), 0);
+
+				cout << "Closing socket--Waiting for new request\n\n";
+
+				//Closes socket, then reopnes socket to listen for new connections
+
+				closesocket(acceptSocket);
+
+				// Create a new socket to listen for client connections
+				listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+				if (listenSocket == INVALID_SOCKET)
+				{
+					cerr << "ERROR: Cannot create socket\n";
+					WSACleanup();
+					return 1;
+				}
+
+				// Attempt to bind to the port.
+				if (bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+				{
+					cerr << "ERROR: Cannot bind to port\n";
+					cleanup(listenSocket);
+					return 1;
+				}
+
+				// Start listening for incoming connections
+				if (listen(listenSocket, 1) == SOCKET_ERROR)
+				{
+					cerr << "ERROR: Problem with listening on socket\n";
+					cleanup(listenSocket);
+					return 1;
+				}
+
+				cout << "\nWaiting for connections...\n";
+
+				// Accept incoming connection.  Program pauses here until
+				// a connection arrives.
+				acceptSocket = accept(listenSocket, NULL, NULL);
+
+				// For this program, the listen socket is no longer needed so it will be closed
+				closesocket(listenSocket);
+
+				cout << "Connected...\n\n";
+
 				if (iSend == SOCKET_ERROR)
 				{
 					cerr << "ERROR: Failed to send message\n";
